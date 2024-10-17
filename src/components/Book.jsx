a081ef69bd3f16ca982c77714f5fbdedf584855b
Noman Dhoni <nomandhoni@yahoo.com>
2024-10-17 14:01:27 +0600

    Repositioned the add button

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,9 +5,9 @@ const Book = ({ book }) => {
     <div className="relative w-full h-[500px] bg-slate-50 shadow-sm mx-auto dark:bg-dark-tertiary rounded overflow-hidden rounded-t-2xl">
       <div className="relative">
         {/* Container with a fixed aspect ratio for consistent height */}
-        <div className="w-full h-72 bg-gray-200 flex items-center justify-center">
+        <div className="w-full h-72 bg-gray-200 flex items-center justify-center relative overflow-hidden">
           <img
-            className="w-full h-full object-fill "
+            className="w-full h-full object-fill"
             src={book.formats["image/jpeg"]}
             alt={`Cover of ${book.title}`}
             onError={(e) => {
@@ -15,16 +15,28 @@ const Book = ({ book }) => {
               e.target.src = "path/to/placeholder-image.jpg"; // Add a placeholder in case of missing images
             }}
           />
+
+          {/* Top overlay */}
+          {/* <div className="absolute top-0 left-0 w-full h-1/6 bg-gradient-to-b from-black/60 to-transparent z-10"></div> */}
+
+          {/* Bottom overlay */}
+          <div className="absolute bottom-0 left-0 w-full h-1/5 bg-gradient-to-t from-black/60 to-transparent z-10"></div>
         </div>
 
         {/* Heart button for favorites */}
         <button
-          className="absolute top-2 right-2 bg-iconBg p-2 rounded-full shadow hover:scale-105 transition-transform duration-300"
+          className="absolute bottom-2 right-2 bg-iconBg p-1 rounded-full shadow hover:scale-105 transition-transform duration-300 z-20 flex hover:space-x-1 items-center justify-center hover:px-3 group"
           aria-label="Add to favorites"
         >
+          {/* 'Add' text will only appear on hover */}
+          <span className="text-black hidden group-hover:block transition-opacity duration-700">
+            Add
+          </span>
           <img src={heart} alt="Add to favorites" className="h-6 w-6" />
         </button>
-        <p className="absolute bottom-2 left-2 text-primary text-xs mt-2">
+
+        {/* Book ID */}
+        <p className="absolute bottom-2 left-2 text-primary text-sm font-bold mt-2 z-20">
           {book.id}
         </p>
       </div>
--- src/components/BooksCollection.jsx
+++ src/components/BooksCollection.jsx
@@ -11,14 +11,14 @@ const BooksCollection = ({
   return (
     <div className="w-full mx-auto px-8 sm:px-4 md:px-6 lg:px-8">
       {loading ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 sm:gap-3 md:gap-4 lg:gap-6 xl:gap-7">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 sm:gap-3 md:gap-4 lg:gap-6">
           {/* Show skeleton loader for each card while loading */}
           {[...Array(12)].map((_, idx) => (
             <SkeletonLoader key={idx} />
           ))}
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 sm:gap-3 md:gap-4 lg:gap-6 xl:gap-7">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-2 sm:gap-3 md:gap-4 lg:gap-6">
           {books.map((book) => (
             <Book key={book.id} book={book} />
           ))}
